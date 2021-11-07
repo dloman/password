57768035f03f26e2e85b14d6730c8d9c724ccc8d
Dan Loman <danloman@amazon.com>
2021-11-06 20:10:02 -0700

    added a bunch of things

---
+++ lib/main.dart
@@ -0,0 +1,170 @@
+import 'package:crypto/crypto.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Password Generator'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _PasswordController = TextEditingController();
+  final _SiteController = TextEditingController();
+
+  String addSpecialCharactersAndCapitalLetters(String input) {
+    const specialCharacters = ["!", "@", "#", "\$", "%", "^", "&", "*", "(", ")"];
+
+    isNumeric(string) => num.tryParse(string) != null;
+
+    var digits = [];
+    var characters = [];
+
+    for(int i=0; i< input.length; i++) {
+      if (isNumeric(input[i])) {
+        digits.add(input[i]);
+      } else {
+        characters.add(input[i]);
+      }
+    }
+    print(digits.toString() + digits.length.toString()+ characters.toString());
+
+    if (digits.length > 1) {
+      input = input.replaceFirst(digits[0], specialCharacters[int.parse(digits[0])]);
+      print(input);
+    } else {
+      print (specialCharacters[int.parse(digits[0])]);
+      input = '!2' + input.substring(2);
+    }
+
+    if (characters.length  > 1) {
+      input = input.replaceFirst(characters[0], characters[0].toUpperCase());
+    } else {
+      input = input.substring(0, input.length - 2) + 'dD';
+    }
+
+    return input;
+  }
+
+  String generatePassword(String password, String site) {
+    var input = password + site + '\n';
+    input = input.replaceAll(' ', '');
+    input.toLowerCase();
+
+    var bytes = utf8.encode(input);
+
+    var digest = sha1.convert(bytes);
+
+    return addSpecialCharactersAndCapitalLetters(digest.toString().substring(0,10));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Enter Password'
+                ),
+                obscureText: true,
+                autocorrect: false,
+                textAlign: TextAlign.center,
+                controller: _PasswordController,
+            ),
+           TextFormField(
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    labelText: 'Enter Site'
+                ),
+                textAlign: TextAlign.center,
+                controller: _SiteController,
+            ),
+            RaisedButton(
+                onPressed: () {
+                  var password = generatePassword(_PasswordController.text, _SiteController.text);
+                  Clipboard.setData(ClipboardData(text: password));
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('copied ' + password + ' to clipboard'))
+                  );
+                },
+                child: Text("Generate Password"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
